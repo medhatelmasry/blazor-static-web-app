cce3da22862eda8be7b0b084c0d14a79e5b61b65
Medhat Elmasry <medhat@elmasry.ca>
2020-07-20 19:54:49 -0700

    BaseURL /

--- blazor-app/Models/Constants.cs
+++ blazor-app/Models/Constants.cs
@@ -2,7 +2,8 @@ namespace blazor_app.Models {
     public class Constants {
         public static string BaseURL {
             get {
-                return "http://localhost:7071/";
+                //return "http://localhost:7071/";
+                return "/";
             }
         }
     }
